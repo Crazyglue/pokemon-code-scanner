ff62248cdbfd87462fa772b26fc14f0ae1286c7e
Eric Dobroveanu <edbz2k2@gmail.com>
2025-02-09 16:45:20 -0500

    feat: add buy-me-a-coffee button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,13 @@
 	</main>
 
 	<footer>
-		<p>Buy me a coffee: TODO</p>
+		<a href="https://www.buymeacoffee.com/rockergaming" target="_blank"
+			><img
+				src="https://cdn.buymeacoffee.com/buttons/v2/default-green.png"
+				alt="Buy Me A Coffee"
+				style="height: 60px !important;width: 217px !important;"
+			/></a
+		>
 	</footer>
 </div>
 
